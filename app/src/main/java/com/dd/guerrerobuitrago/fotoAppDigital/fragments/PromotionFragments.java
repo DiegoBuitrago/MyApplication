79bf71cb2b862a9151beaff6da9c726e2793c2e4
diegoguerreroborda <diego.guerrero02@uptc.edu.co>
2020-01-09 06:15:10 -0500

    ver 2.3.312

--- app/src/main/java/com/dd/guerrerobuitrago/fotoAppDigital/fragments/PromotionFragments.java
+++ app/src/main/java/com/dd/guerrerobuitrago/fotoAppDigital/fragments/PromotionFragments.java
@@ -1,5 +1,8 @@
 package com.dd.guerrerobuitrago.fotoAppDigital.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,9 +10,11 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.dd.guerrerobuitrago.fotoAppDigital.R;
 import com.dd.guerrerobuitrago.fotoAppDigital.adapters.PromotionRVAdapter;
@@ -29,11 +34,14 @@ public class PromotionFragments extends Fragment {
     private PromotionRVAdapter rvAdapter;
     private FloatingActionButton btnFloat;
 
+//    private ImageView imagen;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View promotionView = inflater.inflate(R.layout.fragment_promotion_fragments, container, false);
+//        imagen = (ImageView) promotionView.findViewById(R.id.iv_promotion);
         initComponents(promotionView);
         return promotionView;
     }
@@ -41,10 +49,12 @@ public class PromotionFragments extends Fragment {
     private void initComponents(View view) {
         btnFloat = view.findViewById(R.id.btn_float);
         rvPromotion = view.findViewById(R.id.rv_promotion);
+//        imagen = (ImageView) view.findViewById(R.id.iv_promotion);
         initRecyclerView();
         btnFloat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                loadImage();
                 promotionsList.add(new Promotion("Component"));
                 rvAdapter.notifyItemInserted(promotionsList.size());
             }
@@ -55,8 +65,8 @@ public class PromotionFragments extends Fragment {
         promotionsList = new ArrayList<>();
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(),
-                        RecyclerView.VERTICAL,
-                        false);
+                RecyclerView.VERTICAL,
+                false);
         rvAdapter = new PromotionRVAdapter(promotionsList);
         rvAdapter.setListener(new PromotionRVAdapter.onItemClickListener() {
             @Override
@@ -71,7 +81,6 @@ public class PromotionFragments extends Fragment {
             @Override
             public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
-
                 if (!recyclerView.canScrollVertically(1) && promotionsList.size() > 2) {
                     btnFloat.hide();
                 }else {
@@ -79,6 +88,22 @@ public class PromotionFragments extends Fragment {
                 }
             }
         });
+    }
+
+    private void loadImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent, "Seleccione la aplicacion"), 10);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK){
+            Uri path = data.getData();
+            ImageView image = getView().findViewById(R.id.iv_promotion);
+            image.setImageURI(path);
+        }
     }
 
     public static PromotionFragments newInstance(){
