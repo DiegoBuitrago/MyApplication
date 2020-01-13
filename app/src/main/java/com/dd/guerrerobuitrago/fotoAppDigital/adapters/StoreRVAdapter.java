c55a1198127a171f2489fd17ed3647e5391294ca
DiegoBuitrago <alejobq@hotmail.com>
2020-01-13 16:51:02 -0500

    2.5.230

---
+++ app/src/main/java/com/dd/guerrerobuitrago/fotoAppDigital/adapters/StoreRVAdapter.java
@@ -0,0 +1,83 @@
+package com.dd.guerrerobuitrago.fotoAppDigital.adapters;
+
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dd.guerrerobuitrago.fotoAppDigital.R;
+import com.dd.guerrerobuitrago.fotoAppDigital.models.Product;
+
+import java.util.ArrayList;
+
+public class StoreRVAdapter extends RecyclerView.Adapter<StoreRVAdapter.StoreViewHolder>{
+    private ArrayList<Product> products;
+    private onItemClickListener listener;
+
+    public interface onItemClickListener{
+        void onDeleteClick(int position);
+    }
+
+    public void setListener(onItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    static class StoreViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvProductName;
+        TextView tvProductDes;
+        ImageView imageProduct;
+        ImageButton btnDelete;
+
+        public StoreViewHolder(@NonNull View itemView, final onItemClickListener listener) {
+            super(itemView);
+            tvProductName = itemView.findViewById(R.id.tv_products_name);
+            imageProduct = itemView.findViewById(R.id.iv_product);
+            tvProductDes = itemView.findViewById(R.id.tv_products_des);
+            btnDelete = itemView.findViewById(R.id.btn_delete_product);
+            btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(listener != null){
+                        int position = getAdapterPosition();
+                        if(position != RecyclerView.NO_POSITION){
+                            listener.onDeleteClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public StoreRVAdapter(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    @NonNull
+    @Override
+    public StoreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View cardViewStore = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_store, parent, false);
+        StoreViewHolder viewHolder = new StoreViewHolder(cardViewStore, listener);
+        return viewHolder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull StoreRVAdapter.StoreViewHolder holder, int position) {
+        Product product = products.get(position);
+        holder.tvProductName.setText(product.getName());
+        holder.tvProductDes.setText(product.getDescription());
+        holder.imageProduct.setImageURI(Uri.parse(product.getPhoto()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+}
--- app/src/main/java/com/dd/guerrerobuitrago/fotoAppDigital/fragments/StoreFragment.java
+++ app/src/main/java/com/dd/guerrerobuitrago/fotoAppDigital/fragments/StoreFragment.java
@@ -1,26 +1,49 @@
 package com.dd.guerrerobuitrago.fotoAppDigital.fragments;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.dd.guerrerobuitrago.fotoAppDigital.R;
+import com.dd.guerrerobuitrago.fotoAppDigital.adapters.StoreRVAdapter;
+import com.dd.guerrerobuitrago.fotoAppDigital.models.Product;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StoreFragment extends Fragment {
 
+    private ArrayList<Product> productList;
+
+    private RecyclerView rvStore;
+    private StoreRVAdapter rvAdapter;
     private FloatingActionButton btnFloatStore;
+
+    private EditText txtInputNameProduct;
+    private EditText txtInputDesProduct;
+
+    private ImageView imageCustomDialog;
+
+    private Uri imageUri;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,9 +54,12 @@ public class StoreFragment extends Fragment {
         return storeView;
     }
 
-
     private void initComponents(View view) {
         btnFloatStore = view.findViewById(R.id.btn_float_store);
+        rvStore = view.findViewById(R.id.rv_store);
+        this.imageUri = Uri.parse("");
+
+        initRecyclerView();
         btnFloatStore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,12 +68,40 @@ public class StoreFragment extends Fragment {
         });
     }
 
+    private void initRecyclerView(){
+        productList = new ArrayList<>();
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL,
+                false);
+        rvAdapter = new StoreRVAdapter(productList);
+        rvAdapter.setListener(new StoreRVAdapter.onItemClickListener() {
+            @Override
+            public void onDeleteClick(int position) {
+                productList.remove(position);
+                rvAdapter.notifyItemRemoved(position);
+            }
+        });
+        rvStore.setLayoutManager(layoutManager);
+        rvStore.setAdapter(rvAdapter);
+        rvStore.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (!recyclerView.canScrollVertically(1) && productList.size() > 2) {
+                    btnFloatStore.hide();
+                } else {
+                    btnFloatStore.show();
+                }
+            }
+        });
+    }
+
     private void addProductToList(View view){
         AlertDialog.Builder alert = new AlertDialog.Builder(view.getContext());
         View mView = getLayoutInflater().inflate(R.layout.custom_dialog_add_store, null);
 
-        EditText txtInputNameProduct = mView.findViewById(R.id.txt_input_name_product);
-        EditText txtDescProduct = mView.findViewById(R.id.txt_input_desc_product);
+        txtInputNameProduct = mView.findViewById(R.id.txt_input_name_product);
+        txtInputDesProduct = mView.findViewById(R.id.txt_input_desc_product);
+        imageCustomDialog = mView.findViewById(R.id.imageView4);
         Button btnAddImage = mView.findViewById(R.id.btn_choose_img_product);
         Button btnCancel = mView.findViewById(R.id.btn_cancel_add_product);
         Button btnAccept = mView.findViewById(R.id.btn_add_product_to_list);
@@ -66,7 +120,8 @@ public class StoreFragment extends Fragment {
         btnAccept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "Falta hacer xd", Toast.LENGTH_LONG).show();
+                productList.add(new Product(txtInputNameProduct.getText().toString(),txtInputDesProduct.getText().toString(), imageUri.toString()));
+                rvAdapter.notifyItemInserted(productList.size());
                 alertDialog.dismiss();
             }
         });
@@ -74,20 +129,35 @@ public class StoreFragment extends Fragment {
         btnAddImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                loadImage();
             }
         });
         alertDialog.show();
-
     }
 
     public static StoreFragment newInstance(){
-
         Bundle args = new Bundle();
-
         StoreFragment fragment = new StoreFragment();
         fragment.setArguments(args);
         return fragment;
     }
 
+    private void loadImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent, "Seleccione la aplicacion"), 10);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            this.imageUri = data.getData();
+            this.imageCustomDialog.setImageURI(imageUri);
+
+//            Uri path = data.getData();
+//            ImageView image = getView().findViewById(R.id.iv_promotion);
+//            image.setImageURI(path);
+        }
+    }
 }
